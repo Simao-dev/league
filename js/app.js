d1a9115c7c9f5d3d0d9b212786c867427128abd8
Pedro Simão <pedrosimaocontato@gmail.com>
2024-09-06 23:14:21 -0300

    adicionado imagens
    adicionado a variável imagens e adicionado as imagens no banco

--- js/app.js
+++ js/app.js
@@ -9,34 +9,35 @@ function pesquisar() {
         section.innerHTML = "<p> Digite um nome valido</p>"
         return
     }
-
+     
     campoPesquisa = campoPesquisa.toLowerCase()
-
+    // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
     let nome = "";
     let descricao = "";
     let rota = "";
     let classe = "";
     let tag = "";
-    
+    // Itera sobre cada dado da lista de dados
     for (let dado of dados) {
         nome = dado.nome.toLowerCase()
         descricao = dado.descricao.toLowerCase()
         rota = dado.rota.toLowerCase()
         classe = dado.classe.toLowerCase()
         tag = dado.tag.toLowerCase()
-
+         // se titulo includes campoPesquisa
         if (nome.includes(campoPesquisa) || 
             descricao.includes(campoPesquisa) || 
             rota.includes(campoPesquisa) ||
             classe.includes(campoPesquisa) ||
             tag.includes(campoPesquisa)) {
-
+                // cria um novo elemento
                 resultados += `
                   <div class="item-resultado">
                     <h2>
                         ${dado.nome}
                     </h2>
+                    <img src="${dado.imagem}" alt="${dado.nome}">
                     <p class="descricao-meta">${dado.descricao}</p>
                     <h3>extras</h3>
                     <p>Rotas:${dado.rota}</p>
@@ -49,6 +50,6 @@ function pesquisar() {
     if (!resultados) {
         resultados = "<p>Nada foi encontrado</p>"
     }
-    
+    // Atribui os resultados gerados à seção HTML
     section.innerHTML = resultados;
 }
